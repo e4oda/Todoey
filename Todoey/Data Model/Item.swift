88e68d7c8ec148d17134a137536ed829624c4805
user01 <user01@abc.com>
2019-01-07 22:58:54 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by macfuku on 2019/01/07.
+//  Copyright © 2019 macf. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
