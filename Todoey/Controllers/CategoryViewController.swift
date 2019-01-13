acb187fd347b46c9b07f107bc253a31ab21852d2
user01 <user01@abc.com>
2019-01-13 09:24:18 +0900

    CoreData Challenge: CategoryViewController

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by macfuku on 2019/01/09.
+//  Copyright © 2019 macf. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+}
